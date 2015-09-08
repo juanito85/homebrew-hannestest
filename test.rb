94c03cb65520c2958e47c10b6f2035d755992312
Doberer, Johannes <johannes.doberer@sap.com>
2015-09-08 11:47:07 +0200

    no message

--- test.rb
+++ test.rb
@@ -1,16 +1,14 @@
 require 'formula'
 
 class Test < Formula
-  homepage 'https://github.com/juanito85/test'
+    homepage 'https://github.com/juanito85/homebrew-hannestest'
   head 'https://github.com/hybris/builder-cli/tree/feature/homebrew/dist/homebrew-builder'
   url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/test.tgz'
   version '0.0.1'
   sha1 'afd2073e34c1b092e298905a98ceee21166e3540'
-    
-  depends_on :arch => :x86_64
 
   def install
-      bin.install 'test'
+      bin.install 'homebrew-builder'
   end
 
   test do
