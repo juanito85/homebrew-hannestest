e96425fcf58c8e7288321b4949d0f21a3b62a6ad
Doberer, Johannes <johannes.doberer@sap.com>
2015-09-08 10:36:43 +0200

    no message

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Test < Formula
   homepage 'https://github.com/juanito85/test'
-  head 'https://github.com/juanito85/test'
+  head 'https://github.com/hybris/builder-cli/tree/feature/homebrew/dist/homebrew-builder'
   url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/test.tgz'
   version '0.0.1'
   sha1 'afd2073e34c1b092e298905a98ceee21166e3540'
