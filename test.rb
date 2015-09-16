562d75587d071a7dd1b64cdb44a5dfdf26b1dba4
Doberer, Johannes <johannes.doberer@sap.com>
2015-09-16 15:16:48 +0200

    test

--- test.rb
+++ test.rb
@@ -1,18 +1,19 @@
 require 'formula'
 
 class Hannestest < Formula
-    homepage 'https://github.com/hybris/builder-cli/tree/feature/homebrew/dist/homebrew-builder'
-  head 'https://github.com/hybris/builder-cli/tree/feature/homebrew/dist/homebrew-builder'
-  url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/test.tgz'
+  homepage 'https://github.com/hybris/builder-cli/tree/feature/homebrew/dist/homebrew-builder'
+  head 'https://github.com/hybris/builder-cli/blob/feature/homebrew/dist/homebrew-builder/builder.tgz?raw=true'
+  url  'https://github.com/hybris/builder-cli/blob/feature/homebrew/dist/homebrew-builder/builder.tgz?raw=true'
   version '0.0.1'
   sha1 'afd2073e34c1b092e298905a98ceee21166e3540'
 
   def install
-      bin.install 'homebrew-builder'
+      bin.install 'builder'
   end
 
   test do
-    system "#{bin}/homebrew-builder"
+    system "#{bin}/builder"
   end
 end
 
+    
